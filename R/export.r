5f1cabe6c564b1f0974dae80114a23be5c284fd9
Yuhao Lin <lyh970817@yandex.com>
2020-03-02 14:41:54 +0000

    Change add_numeric in export

--- R/export.r
+++ R/export.r
@@ -39,7 +39,6 @@ GLAD_export <- function(data_cleaned, data_raw, questionnaire, dirpath, googlesh
   dir.create(file.path(dirpath, "feather"), showWarnings = FALSE)
 
   data_cleaned_csv <- to_csv(data_cleaned, data_raw)
-  data_cleaned_bin <- add_numeric(data_cleaned, data_raw)
 
   if (rename == TRUE) {
     questionnaire <- paste0(questionnaire, "_Renamed")
@@ -50,15 +49,18 @@ GLAD_export <- function(data_cleaned, data_raw, questionnaire, dirpath, googlesh
       to = "easyname"
     )
 
+
     csv_name <- paste0("csv_renamed/", questionnaire, ".csv")
     csv_path <- paste0(dirpath, csv_name)
     write_csv(x = data_easyname_csv, path = csv_path)
 
-    data_easyname_bin <- GLAD_rename(data_cleaned_bin,
+    data_easyname_bin <- GLAD_rename(data_cleaned,
       googlesheet = googlesheet,
       from = "newvar",
       to = "easyname"
     )
+
+    data_easyname_bin <- add_numeric(data_easyname_bin, data_raw)
 
     rds_name <- paste0("rds_renamed/", questionnaire, ".rds")
     rds_path <- paste0(dirpath, rds_name)
@@ -68,6 +70,9 @@ GLAD_export <- function(data_cleaned, data_raw, questionnaire, dirpath, googlesh
     feather_path <- paste0(dirpath, feather_name)
     write_feather(data_easyname_bin, path = feather_path)
   } else {
+    data_cleaned_csv <- to_csv(data_cleaned, data_raw)
+    data_cleaned_bin <- add_numeric(data_cleaned, data_raw)
+
     csv_name <- paste0("csv/", questionnaire, ".csv")
     csv_path <- paste0(dirpath, csv_name)
     write_csv(x = data_cleaned_csv, path = csv_path)
